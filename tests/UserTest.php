ca6f5bfc14974dba9bb0a1222833c4e599bf2182
marvin <marvin@marvinisaac.com>
2020-06-07 05:54:04 +0800

    :construction: Setup tests for user registration
    Also update pre-commit script to allow failed tests during commit
    to allow support TDD workflow

---
+++ tests/UserTest.php
@@ -0,0 +1,76 @@
+<?php
+
+    namespace Test;
+
+    use \PHPUnit\Framework\TestCase;
+    use \Slim\App;
+    use Api\Api;
+    use Test\ApiTestHelper as Helper;
+
+class UserTest extends TestCase
+{
+    protected App $api;
+    protected Helper $helper;
+
+    public function setUp() : void
+    {
+        $_ENV['PHP_ENVIRONMENT'] = 'TEST';
+        $this->api = (new Api())->get();
+        $this->helper = new Helper();
+    }
+
+    public function testIncompleteRequestsShouldReturn400WithErrorMessage() : void
+    {
+        $requestsIncomplete = [
+            [
+                'body' => [
+                    'username' => '',
+                    'passsword' => '',
+                ],
+                'error_message' => 'Missing input: username, password',
+            ], [
+                'body' => [
+                    'username' => 'username',
+                ],
+                'error_message' => 'Missing input: password',
+            ], [
+                'body' => [
+                    'password' => 'password',
+                ],
+                'error_message' => 'Missing input: username',
+            ],
+        ];
+        foreach ($requestsIncomplete as $incomplete) {
+            $request = $this->helper->prepareRequest('POST', '/user', $incomplete['body']);
+            $this->api->getContainer()['request'] = $request;
+            
+            $response = $this->api->run(true);
+            $responseBody = json_decode((string)$response->getBody(), true);
+            $responseStatus = $response->getStatusCode();
+    
+            $this->assertSame(400, $responseStatus);
+            $this->assertArrayHasKey('error_message', $responseBody);
+            $this->assertSame($incomplete['error_message'], $responseBody['error_message']);
+        }
+    }
+
+    public function testCompleteRequestsShouldReturn200WithOriginalInput() : void
+    {
+        $requestComplete = [
+            'username' => 'username',
+            'password' => 'password',
+        ];
+        $request = $this->helper->prepareRequest('POST', '/user', $requestComplete);
+        $this->api->getContainer()['request'] = $request;
+        
+        $response = $this->api->run(true);
+        $responseBody = json_decode((string)$response->getBody(), true);
+        $responseStatus = $response->getStatusCode();
+
+        $this->assertSame(200, $responseStatus);
+        $this->assertArrayHasKey('username', $responseBody);
+        $this->assertArrayHasKey('password', $responseBody);
+        $this->assertSame($requestComplete['username'], $responseBody['username']);
+        $this->assertSame($requestComplete['password'], $responseBody['password']);
+    }
+}
