013d860f3234126835b9440cdc2b93d962388138
marvin <marvin@marvinisaac.com>
2020-06-07 11:49:41 +0800

    🚧 Setup tests for reading user information

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -11,12 +11,14 @@ class UserTest extends TestCase
 {
     protected App $api;
     protected Helper $helper;
+    protected string $testUsername;
 
     public function setUp() : void
     {
         $_ENV['PHP_ENVIRONMENT'] = 'TEST';
         $this->api = (new Api())->get();
         $this->helper = new Helper();
+        $this->testUsername = 'test' . time();
     }
 
     public function testIncompleteRequestsShouldReturn400WithErrorMessage() : void
@@ -57,7 +59,7 @@ class UserTest extends TestCase
     public function testCompleteRequestsShouldReturn200WithUsername() : void
     {
         $requestComplete = [
-            'username' => 'test' . time(),
+            'username' => $this->testUsername,
             'password' => 'password',
         ];
         $request = $this->helper->prepareRequest('POST', '/user', $requestComplete);
@@ -70,5 +72,35 @@ class UserTest extends TestCase
         $this->assertSame(200, $responseStatus);
         $this->assertArrayHasKey('username', $responseBody);
         $this->assertSame($requestComplete['username'], $responseBody['username']);
+    }
+
+    public function testReadAllRecordsShouldReturn200WithAllUsers() : void
+    {
+        $request = $this->helper->prepareRequest('GET', '/user');
+        $this->api->getContainer()['request'] = $request;
+        
+        $response = $this->api->run(true);
+        $responseBody = json_decode((string)$response->getBody(), true);
+        $responseStatus = $response->getStatusCode();
+
+        $this->assertSame(200, $responseStatus);
+        foreach ($responseBody as $user) {
+            $this->assertArrayHasKey('username', $user);
+            $this->assertArrayHasKey('created_at', $user);
+        }
+    }
+
+    public function testReadSingleRecordShouldReturn200WithSpecifiedUser() : void
+    {
+        $request = $this->helper->prepareRequest('GET', '/user/' . $this->testUsername);
+        $this->api->getContainer()['request'] = $request;
+        
+        $response = $this->api->run(true);
+        $responseBody = json_decode((string)$response->getBody(), true);
+        $responseStatus = $response->getStatusCode();
+
+        $this->assertSame(200, $responseStatus);
+        $this->assertArrayHasKey('username', $responseBody);
+        $this->assertArrayHasKey('created_at', $responseBody);
     }
 }
